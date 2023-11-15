f0946c0ea7571096db4d1e9bcd49ed7b716ddf19
Pranav <pranav.techiegeek@gmail.com>
2023-11-15 17:03:40 +0530

    ready

--- Main.java
+++ Main.java
@@ -1,6 +1,10 @@
-
 class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        // Test cases
+        System.out.println("2000 is leap year: " + isLeapYear(2000)); // Should return true
+        System.out.println("1989 is leap year: " + isLeapYear(1989)); // Should return false
+        System.out.println("2100 is leap year: " + isLeapYear(2100)); // Should return false
     }
+
+    // Implement the "isLeapYear" method
 }
