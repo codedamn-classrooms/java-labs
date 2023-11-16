eefbfd26ebc04cc49367d32b18bb6a1fd922378e
Pranav <pranav.techiegeek@gmail.com>
2023-11-16 15:46:10 +0530

    ready

--- Main.java
+++ Main.java
@@ -1,6 +1,12 @@
-
 class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        // Example test cases
+        System.out.println("Is 0 prime? " + isPrimeNumber(0)); // Expected: false
+        System.out.println("Is 3 prime? " + isPrimeNumber(3)); // Expected: true
+        System.out.println("Is 2 prime? " + isPrimeNumber(2)); // Expected: true
+        System.out.println("Is 99 prime? " + isPrimeNumber(99)); // Expected: false
+        System.out.println("Is -5 prime? " + isPrimeNumber(-5)); // Expected: false
     }
+
+    // Create the "isPrimeNumber" method here. Accepts Integer value as parameter and should return a boolean value
 }
