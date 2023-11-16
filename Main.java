dd039221c51674d34ac6f176b203bc0191e3a555
Pranav <pranav.techiegeek@gmail.com>
2023-11-16 16:09:08 +0530

    ready

--- Main.java
+++ Main.java
@@ -1,6 +1,11 @@
-
 class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        // Test cases
+        System.out.println("sumOfIntegers(10) = " + sumOfIntegers(10)); // Expected output: 55
+        System.out.println("sumOfIntegers(0) = " + sumOfIntegers(0));   // Expected output: 0
+        System.out.println("sumOfIntegers(7) = " + sumOfIntegers(7));   // Expected output: 28
+        System.out.println("sumOfIntegers(18) = " + sumOfIntegers(18)); // Expected output: 171
     }
+
+    // Create the "sumOfIntegers" method which takes an integer and returns an integer sum 
 }
