4625f41bc13a75373b0aa36f98a32beb31c59e18
Pranav <pranav.techiegeek@gmail.com>
2023-11-16 17:04:23 +0530

    ready

--- Main.java
+++ Main.java
@@ -1,6 +1,10 @@
-
 class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        // Test cases to verify the implementation
+        System.out.println(isPalindrome("racecar")); // Should return true
+        System.out.println(isPalindrome("A man, a plan, a canal ? Panama")); // Should return true
+        System.out.println(isPalindrome("codedamn")); // Should return false
     }
+    
+    // Implement the "isPalindrome" method here
 }
