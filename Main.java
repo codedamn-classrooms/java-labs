f75cee2be5dfaab84c6532501a74708070594a86
Pranav <pranav.techiegeek@gmail.com>
2023-11-16 17:19:34 +0530

    ready

--- Main.java
+++ Main.java
@@ -5,7 +5,7 @@ class Main {
         System.out.println(binaryToDecimal("111")); // Should output 7
         System.out.println(binaryToDecimal("100")); // Should output 4
         System.out.println(binaryToDecimal("1010101")); // Should output 85
-        System.out.println(binaryToDecimal("10a1")); // Uncomment to test invalid input
+        // System.out.println(binaryToDecimal("10a1")); // Uncomment to test invalid input
     }
 
    // Implement the "binaryToDecimal" method here
