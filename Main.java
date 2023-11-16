5e3f326a81655326d057b40a1cf4322824538578
Pranav <pranav.techiegeek@gmail.com>
2023-11-16 17:18:57 +0530

    ready

--- Main.java
+++ Main.java
@@ -1,6 +1,12 @@
-
 class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        // Example function calls
+        System.out.println(binaryToDecimal("1010")); // Should output 10
+        System.out.println(binaryToDecimal("111")); // Should output 7
+        System.out.println(binaryToDecimal("100")); // Should output 4
+        System.out.println(binaryToDecimal("1010101")); // Should output 85
+        System.out.println(binaryToDecimal("10a1")); // Uncomment to test invalid input
     }
+
+   // Implement the "binaryToDecimal" method here
 }
