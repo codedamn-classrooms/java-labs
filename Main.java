7a713e81d0cb8a23b1f6cfa1a2aee2c47c769cf1
Pranav <pranav.techiegeek@gmail.com>
2023-11-29 15:25:39 +0530

    ready

--- Main.java
+++ Main.java
@@ -1,6 +1,11 @@
-
 class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        // Test cases
+        System.out.println("Sum of digits (5): " + sumOfDigits(5));    // Single-digit
+        System.out.println("Sum of digits (34): " + sumOfDigits(34));  // Double-digit
+        System.out.println("Sum of digits (129): " + sumOfDigits(129)); // Triple-digit
+        System.out.println("Sum of digits (54321): " + sumOfDigits(54321)); // Five-digit
     }
+
+    // Create the sumOfDigits method here
 }
