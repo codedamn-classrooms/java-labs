2267d3be51978a7065a8bc728fa941dcc2deabf4
Pranav <pranav.techiegeek@gmail.com>
2023-11-29 15:41:11 +0530

    ready

--- Main.java
+++ Main.java
@@ -1,6 +1,29 @@
 
 class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        // Example test cases
+        int[] array1 = {1, 2, 3};
+        int[] array2 = {4, 5, 6};
+        int[] result1 = commonElements(array1, array2);
+        printArray(result1); // Expected: []
+
+        int[] array3 = {1, 2, 3, 4};
+        int[] array4 = {3, 4, 5, 6};
+        int[] result2 = commonElements(array3, array4);
+        printArray(result2); // Expected: [3, 4]
     }
+
+    // Utility method to print arrays for testing
+    private static void printArray(int[] array) {
+        System.out.print("[");
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i]);
+            if (i < array.length - 1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println("]");
+    }
+
+    // Create the "commonElements" method here
 }
