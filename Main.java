2a85b501c2474e15cd4bc2cff70c6f072599a411
Pranav <pranav.techiegeek@gmail.com>
2023-11-29 15:32:55 +0530

    ready

--- Main.java
+++ Main.java
@@ -1,6 +1,12 @@
-
 class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        // Test cases
+        System.out.println("isPowerOfTwo(1): " + isPowerOfTwo(1)); // Should return true
+        System.out.println("isPowerOfTwo(5): " + isPowerOfTwo(5)); // Should return false
+        System.out.println("isPowerOfTwo(16): " + isPowerOfTwo(16)); // Should return true
+        System.out.println("isPowerOfTwo(-2): " + isPowerOfTwo(-2)); // Should return false
+        System.out.println("isPowerOfTwo(0): " + isPowerOfTwo(0)); // Should return false
     }
+
+    // create the "isPowerOfTwo" method here
 }
