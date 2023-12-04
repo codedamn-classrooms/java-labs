677f02667273edb3221040a4f873f22758dcc233
Pranav <pranav.techiegeek@gmail.com>
2023-12-04 18:44:49 +0530

    ready

--- Main.java
+++ Main.java
@@ -1,6 +1,13 @@
-
 class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        // Example test cases
+        System.out.println(isAnagram("test", "test")); // true
+        System.out.println(isAnagram("Listen", "Silent")); // true
+        System.out.println(isAnagram("hello", "world")); // false
+        System.out.println(isAnagram("Astronomer", "Moon starer")); // true
+    }
+
+    public static boolean isAnagram(String str1, String str2) {
+        // build the method here
     }
 }
