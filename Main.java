67cf32ef63edd9f97cb1e5044d432b6b549be36a
Pranav <pranav.techiegeek@gmail.com>
2023-12-06 20:26:25 +0530

    ready

--- Main.java
+++ Main.java
@@ -1,6 +1,12 @@
-
 class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        // Test cases
+        System.out.println(generateHexCode(0, 0, 0));    // Black
+        System.out.println(generateHexCode(255, 255, 255)); // White
+        System.out.println(generateHexCode(255, 255, 0));   // Yellow
+    }
+
+    public static String generateHexCode(int red, int green, int blue) {
+        
     }
 }
