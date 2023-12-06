24510cf3554fa16902a1e2d92dc3d5e699db0505
Pranav <pranav.techiegeek@gmail.com>
2023-12-06 20:18:26 +0530

    ready

--- Main.java
+++ Main.java
@@ -1,6 +1,19 @@
-
 class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        // Example usage
+        double miles = 10;
+        double kilometers = 16.0934;
+
+        double convertedToKilometers = convertToKilometers(miles);
+        double convertedToMiles = convertToMiles(kilometers);
+
+        System.out.println(miles + " miles is " + convertedToKilometers + " kilometers.");
+        System.out.println(kilometers + " kilometers is " + convertedToMiles + " miles.");
+    }
+
+    public static double convertToKilometers(double miles) {
+    }
+
+    public static double convertToMiles(double kilometers) {
     }
 }
