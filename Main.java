b88259c6d1ad8753e65980e0dc0eba4e9c7116bf
Pranav <pranav.techiegeek@gmail.com>
2023-12-12 14:39:09 +0530

    ready

--- Main.java
+++ Main.java
@@ -1,6 +1,13 @@
-
 class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        // Testing the calculatedGCD method with the provided challenges
+        System.out.println("GCD of 54 and 24: " + calculatedGCD(54, 24)); // Expected output: 6
+        System.out.println("GCD of 35 and 49: " + calculatedGCD(35, 49)); // Expected output: 7
+        System.out.println("GCD of 1 and 15: " + calculatedGCD(1, 15));   // Expected output: 1
+        System.out.println("GCD of 0 and 15: " + calculatedGCD(0, 15));   // Expected output: 15
+    }
+
+    public static int calculatedGCD(int a, int b) {
+        // Implement the method
     }
 }
