516e5d5db88853740ef326077ce928c400b909cd
Pranav <pranav.techiegeek@gmail.com>
2023-12-12 16:31:40 +0530

    ready

--- Main.java
+++ Main.java
@@ -1,6 +1,18 @@
+import java.util.Stack;
 
 class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        // Test cases
+        System.out.println(verifyBrackets("{[()]}")); // true
+        System.out.println(verifyBrackets("{[(])}")); // false
+        System.out.println(verifyBrackets("")); // true
+        System.out.println(verifyBrackets("{[()]")); // false
+        System.out.println(verifyBrackets("{[()]}}")); // false
+        System.out.println(verifyBrackets("{[(<>)]}")); // true
+        System.out.println(verifyBrackets("{[({})]}")); // true
+    }
+
+    public static boolean verifyBrackets(String input) {
+        // Implement this method
     }
 }
