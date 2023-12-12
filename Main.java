217cc6419595163d285a2b21b6869a81c0a73657
Pranav <pranav.techiegeek@gmail.com>
2023-12-12 12:54:04 +0530

    ready

--- Main.java
+++ Main.java
@@ -1,6 +1,13 @@
-
 class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        // Test cases
+        System.out.println(validateCreditCard("4532015112830366"));  // Challenge 1: Valid Credit Card
+        System.out.println(validateCreditCard("4532015112830368"));  // Challenge 2: Invalid Credit Card
     }
+
+    public static boolean validateCreditCard(String creditCardNumber) {
+        // Implement the Method
+    }
+
+
 }
