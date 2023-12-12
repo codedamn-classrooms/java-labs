5c1aff97f876da2f9aadb71e672bf37c89228e83
Pranav <pranav.techiegeek@gmail.com>
2023-12-12 14:45:00 +0530

    ready

--- Main.java
+++ Main.java
@@ -1,6 +1,20 @@
-
 class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        // Test cases
+        int[] array1 = {1, 2, 3, 4};
+        int[] array2 = {15, 20, 25};
+        int[] array3 = {12, 18};
+        int[] emptyArray = {};
+        int[] singleElementArray = {5};
+
+        System.out.println("LCM of [1, 2, 3, 4]: " + calculateLCM(array1));
+        System.out.println("LCM of [15, 20, 25]: " + calculateLCM(array2));
+        System.out.println("LCM of [12, 18]: " + calculateLCM(array3));
+        System.out.println("LCM of []: " + calculateLCM(emptyArray));
+        System.out.println("LCM of [5]: " + calculateLCM(singleElementArray));
+    }
+
+    public static int calculateLCM(int[] array) {
+        // Write your code here
     }
 }
