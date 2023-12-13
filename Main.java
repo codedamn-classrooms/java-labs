db4d8c0875fce75d53ede7bec46e0b4276d4947e
Pranav <pranav.techiegeek@gmail.com>
2023-12-13 14:25:50 +0530

    ready

--- Main.java
+++ Main.java
@@ -1,6 +1,12 @@
-
 class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        // Example test cases
+        System.out.println(validateISBN("978-1-982181-28-4")); // true
+        System.out.println(validateISBN("778-1-921181-28-4")); // false
+        System.out.println(validateISBN("9780593078754"));     // true
+    }
+
+    public static boolean validateISBN(String isbn) {
+        // Write your code here
     }
 }
