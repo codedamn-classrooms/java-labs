04d224e32c4d4563408a9d229ef5692a8a07529b
Pranav <pranav.techiegeek@gmail.com>
2023-12-13 13:41:01 +0530

    ready

--- Main.java
+++ Main.java
@@ -1,6 +1,14 @@
-
 class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        // Testing the method with different inputs
+        System.out.println(isPythagoreanTriplet(new int[]{5, 4, 3})); // Expected Output: True
+        System.out.println(isPythagoreanTriplet(new int[]{5, 12, 13})); // Expected Output: True
+        System.out.println(isPythagoreanTriplet(new int[]{2, 3, 4})); // Expected Output: False
+        System.out.println(isPythagoreanTriplet(new int[]{7, 25, 24})); // Expected Output: True
+        System.out.println(isPythagoreanTriplet(new int[]{1, 5, 8})); // Expected Output: False
+    }
+
+    public static boolean isPythagoreanTriplet(int[] numbers) {
+        // write your code here 
     }
 }
