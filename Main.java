63cb0df20d1f1589d356ed65ca8fa45c6b800327
Pranav <pranav.techiegeek@gmail.com>
2023-12-13 14:07:48 +0530

    ready

--- Main.java
+++ Main.java
@@ -1,6 +1,15 @@
+import java.util.HashMap;
+import java.util.Map;
 
 class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        // Test cases
+        System.out.println(calculateIPCount("74.125.227.0", 26)); // Expected Output: {lastIP=74.125.227.63, count=64}
+        System.out.println(calculateIPCount("74.125.227.0", 29)); // Expected Output: {lastIP=74.125.227.7, count=8}
+        System.out.println(calculateIPCount("74.125.227.0", 13)); // Expected Output: {lastIP=74.127.255.255, count=524288}
+    }
+
+    public static Map<String, Object> calculateIPCount(String ipAddress, int cidr) {
+        // write your code here
     }
 }
