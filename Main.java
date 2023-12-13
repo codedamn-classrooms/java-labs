89d00f41ff5e5125facd9a2a33d86fa4057163b0
Pranav <pranav.techiegeek@gmail.com>
2023-12-13 13:33:32 +0530

    ready

--- Main.java
+++ Main.java
@@ -1,6 +1,13 @@
-
 class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        // Test cases
+        System.out.println(vigenereCipher("uddgh, ttr hwqeo fdbphliu", "decode")); // Expected Output: space, the final frontier
+        System.out.println(vigenereCipher("May the Force be with you.", "encode")); // Expected Output: Oob xke Rbtqh fh wugj mry.
+        System.out.println(vigenereCipher("Vvh qrsf rphhvwauakbj sxtobos lw whq zqgw plkqya", "decode")); // Expected Output: The most entertaining outcome is the most likely
+    }
+
+    public static String vigenereCipher(String text, String action) {
+        // Write your code here
     }
 }
