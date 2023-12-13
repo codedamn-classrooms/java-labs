f9e64bd4644db0fcc4561d92a34fd54e07f10faa
Pranav <pranav.techiegeek@gmail.com>
2023-12-13 13:14:39 +0530

    ready

--- Main.java
+++ Main.java
@@ -1,6 +1,13 @@
-
 class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        // Test cases
+        System.out.println(compressString("aaabbc")); // Expected Output: a3b2c1
+        System.out.println(compressString("abcd"));   // Expected Output: a1b1c1d1
+        System.out.println(compressString("zzzzz"));  // Expected Output: z5
+        System.out.println(compressString("!@##$%")); // Expected Output: !1@1#2$1%1
+    }
+
+    public static String compressString(String input) {
+        // Write your code here
     }
 }
